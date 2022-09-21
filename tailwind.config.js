b259515a3aa69b7d6a5955298f94da0e35e1c8ee
fadesyphe <pivkin.jura2017@gmail.com>
2022-09-22 00:48:31 +0600

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   content: ['./dist/*.html'],
   theme: {
+    screens: {
+      mobile: { max: '767px' },
+      tablet: { min: '768px', max: '1023px' },
+      laptop: { min: '1023px', max: '1600px' },
+      desktop: { min: '1600px' }
+    },
     extend: {}
   },
+  plugins: [],
   variants: {
     extend: {}
   },
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    app: ['core-js/stable', './src/index.js']
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true,
+    assetModuleFilename: pathData => {
+      const filepath = path
+        .dirname(pathData.filename)
+        .split('/')
+        .slice(1)
+        .join('/')
+      return `${filepath}/[name][ext]`
+    }
   },
   devServer: {
     static: {
@@ -23,7 +35,59 @@ module.exports = {
         test: /\.css$/i,
         include: path.resolve(__dirname, 'src'),
         use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpe?g|gif|mp4)$/i,
+        include: path.resolve(__dirname, ''),
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: './fonts/[name][ext]'
+        }
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-transform-runtime'
+            ]
+          }
+        }
       }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
 }
